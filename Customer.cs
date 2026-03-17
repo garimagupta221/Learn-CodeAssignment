2d8001eafe347aea92111f32ecb055deed79a5d5
Garima Gupta <garima.gupta@intimetec.com>
2026-03-17 12:04:32 +0530

    Refactored the code to follow Law of Demeter

---
+++ Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_object
+{
+    internal class Customer
+    {
+        public Customer(string firstName, string lastName, Wallet wallet)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _myWallet = wallet;
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+
+        public string GetLastName()
+        {
+            return _lastName;
+        }
+
+        public bool Pay(double amount)
+        {
+            return _myWallet.Pay(amount);
+        }
+
+        private string _firstName;
+        private string _lastName;
+        private Wallet _myWallet;
+    }
+}
