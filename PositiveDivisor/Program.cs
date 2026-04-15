8b593a58a8e948ce80926fafc65393db20b577a5
Garima Gupta <garima.gupta@intimetec.com>
2026-04-15 10:35:59 +0530

    Implemented testing

---
+++ PositiveDivisor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveDivisor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var validator = new InputValidator();
+            var calculator = new DivisorCalculator(validator);
+
+            Console.WriteLine("Divisor Calculator");
+            Console.Write("Enter number of test cases: ");
+
+            int testCases = validator.ReadValidInput();
+            for (int index = 0; index < testCases; index++)
+            {
+                int limit = validator.ReadValidInt($"Enter limit for test case {index + 1}: ");
+                try
+                {
+                    int result = calculator.GetTotalDivisors(limit);
+                    Console.WriteLine($"Result: {result}");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+
+        }
+    }
+}
